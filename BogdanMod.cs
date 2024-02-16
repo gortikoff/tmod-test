1140752c8900396ae17c3268f2453e5de5073530
STASYAN <gortikov2004@gmail.com>
2024-02-17 02:12:30 +0300

    second commit

---
+++ BogdanMod.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace BogdanMod
+{
+	public class BogdanMod : Mod
+	{
+	}
+}
---
+++ Items/SwordOfKorkutBey.cs
@@ -0,0 +1,35 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BogdanMod.Items
+{
+	public class SwordOfKorkutBey : ModItem
+	{
+        // The Display Name and Tooltip of this item can be edited in the Localization/en-US_Mods.BogdanMod.hjson file.
+
+		public override void SetDefaults()
+		{
+			Item.damage = 0;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 400;
+			Item.height = 400;
+			Item.useTime = 20;
+			Item.useAnimation = 20;
+			Item.useStyle = 1;
+			Item.knockBack = 600;
+			Item.value = 10000;
+			Item.rare = 2;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.DirtBlock, 1);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
