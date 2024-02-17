7d9487e62c4721a33d50bb786b6a0fdf5257b56f
STASYAN <gortikov2004@gmail.com>
2024-02-17 21:03:09 +0300

    fourth commit

---
+++ Items/SimpleHeartSecond.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BogdanMod.Items
+{
+	public class SimpleHeartSecond : ModItem
+	{
+        // The Display Name and Tooltip of this item can be edited in the Localization/en-US_Mods.BogdanMod.hjson file.
+
+		public override void SetDefaults()
+		{
+			Item.damage = 50;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 18;
+			Item.height = 18;
+			Item.useTime = 1;
+			Item.useAnimation = 30;
+			Item.useStyle = 1;
+			Item.knockBack = 10;
+			Item.value = 10000;
+			Item.rare = 2;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.Wood, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
---
+++ Items/Zulfikar.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BogdanMod.Items
+{
+	public class Zulfikar : ModItem
+	{
+        // The Display Name and Tooltip of this item can be edited in the Localization/en-US_Mods.BogdanMod.hjson file.
+
+		public override void SetDefaults()
+		{
+			Item.damage = 36;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 32;
+			Item.height = 32;
+			Item.useTime = 1;
+			Item.useAnimation = 30;
+			Item.useStyle = 1;
+			Item.knockBack = 10;
+			Item.value = 10000;
+			Item.rare = 5;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.Wood, 10);
+            recipe.AddIngredient(ItemID.Banana, 2);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
