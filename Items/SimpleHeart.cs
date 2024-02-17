1782de731519e40cdee44367cdccaa27f3e32778
STASYAN <gortikov2004@gmail.com>
2024-02-17 16:12:35 +0300

    new commit

--- Items/SimpleHeart.cs
+++ Items/SimpleHeart.cs
@@ -4,16 +4,16 @@ using Terraria.ModLoader;
 
 namespace BogdanMod.Items
 {
-	public class SwordOfKorkutBey : ModItem
+	public class SimpleHeart : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the Localization/en-US_Mods.BogdanMod.hjson file.
 
 		public override void SetDefaults()
 		{
-			Item.damage = 0;
+			Item.damage = 5;
 			Item.DamageType = DamageClass.Melee;
-			Item.width = 400;
-			Item.height = 400;
+			Item.width = 9;
+			Item.height = 9;
 			Item.useTime = 20;
 			Item.useAnimation = 20;
 			Item.useStyle = 1;
